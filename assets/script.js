49fe9d7bfb988f7f14eb449bb9ae19adf797d25b
Sandra <sandra.alpha2010@gmail.com>
2020-05-09 11:51:42 -0700

    added commented out click buttons with ajax calls that dynamically produce divs

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
         // Utelly API - best for determining what streaming service offers a given title
         // Title to search for:
+       
         var title = "space jam";
 
         // Setting up API parameters:
@@ -54,13 +55,84 @@
         
         //////////////////////////////////////////////////////
 
+        /*
+        1. click event for dropdown menu so it toggles the options
+        2. click event for selection, (and preventdefault) that grabs the id (that has the corresponding zomato food code)
+        and then does an ajax call with the id (that has to be turned into an integer first)
+        3. for every selection an ajax call populates the container div with 10 results
+            a. each result gets a set of three (for now) divs + append to element
+        */
+
+        /*
+          click event for dropdown menu so it toggles the options:
+
+          $().on("click", function(){
+
+            $(".dropdown-toggle").dropdown("toggle");
+          });
+
+          if user clicks outside of dropdown it closes:
+
+          $(document).on("click", function(){
+              $(.dropdown-toggle").dropdown("toggle");
+          })
+
+          if use clicks on a selection
+
+          $("#1").on("click", function(e){
+              e.preventDefault();
+
+              var integerId = $(this).attr("id");
+              console.log(intergerId);//want id number as a number but string works too
+
+
+            /*URL includes city id for Sacramento, entity type narrows down what type of location we're looking for restaurants in, category specifies what kind of service
+        we're looking for - category 1 refers to delivery. Count specifies how many results to show on webpage. Cuisines is refrenced by a zomato specific food code*/
+            /*
+            var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=499&entity_type=city&category=1&sort=rating&count=10&cuisines=" + integerId;
+
+            $.ajax({
+                url: queryURL, 
+                method: "GET",
+                headers: {"user-key": "8aeaa8b6fb7879eccc32af42e280f916"} 
+            }).then(function (response) {
+                console.log(response);
+
+                // var for array of restaurants returned by search
+                var restaurant = response.restaurants;
+                
+                // var newDivFood = $("<div id='food-nest'></div>")
+
+                // $("#food-section").append(newDivFood);
+
+                // Loop through and print restaurant info
+                for (var i = 0; i < 10; i++) {
+                    
+                    var name = $("<div class='name'></div>").text("Restaurant name: " + restaurant[i].restaurant.name);
+                    var menu = $("<div class='menu'></div>").text("Restaurant menu: " + restaurant[i].restaurant.menu_url);
+                    var number = $("<div class='number'></div>").text("Restaurant phone number: " + restaurant[i].restaurant.phone_numbers);
+
+                    $("#food-section").append(name, menu, number);
+
+                
+                
+                }
+
+            });
+            
+          });
+
+          
+
+        */
+
 
         // Zomato API
         // Sacramento entity_id = 499
         // entity_type = city
         // Category code for delivery = 1
 
-        /* Cuisine codes: 
+        /* Cuisine codes: 16 
         American = 1, 
         Asian = 3, 
         BBQ = 193, 
@@ -87,43 +159,15 @@
 
         // };
 
-        /*
-        1. click event for dropdown menu so it toggles the options
-        2. click event for selection, (and preventdefault) that grabs the id (that has the corresponding zomato food code)
-        and then does an ajax call with the id (that has to be turned into an integer first)
-        3. for every selection an ajax call populates the container div with 10 results
-            a. each result gets a set of three (for now) divs + append to element*/
-
-
+          
         // var for type of food, have to search by zomato code
         //var cuisineCode = 25;
 
         // API call URL built with given cuisine code
-        /*URL includes city id for Sacramento, entity type narrows down what type of location we're looking for restaurants in, category specifies what kind of service
-        we're looking for - category 1 refers to delivery. Count specifies how many results to show on webpage. Cuisines is refrenced by a zomato specific food code*/
-        var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=499&entity_type=city&category=1&sort=rating&count=10&cuisines=" + 25;
-
-        // AJAX call
-        $.ajax({
-            url: queryURL, 
-            method: "GET",
-            headers: {"user-key": "8aeaa8b6fb7879eccc32af42e280f916"} 
-        }).then(function (response) {
-            console.log(response);
-
-            // var for array of restaurants returned by search
-            var restaurant = response.restaurants;
-
-            // Loop through and print restaurant info
-            for (var i = 0; i < 10; i++) {
-                //jquery .text or .html these results on webpage
-                console.log("Restaurant name: " + restaurant[i].restaurant.name);
-                console.log("Restaurant menu: " + restaurant[i].restaurant.menu_url);
-                console.log("Restaurant phone number: " + restaurant[i].restaurant.phone_numbers);
-            }
-
-        })
-
+        
+        
+        
+        
         //do a drop down menu of the food
         //get all cuisine codes and put in drop down menu - only show the names (grab the names)
         //click event for drop down menu
